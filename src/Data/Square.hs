dc8dcb5b2d45b60e1a4f8413564412d022073e66
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-01-31 21:06:12 +0000

    Multiplication etc

--- src/Data/Square.hs
+++ src/Data/Square.hs
@@ -1,26 +1,55 @@
-{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Data.Square
-  ( Square
-  , create
-  , alterF
-  , fromList
-  , (!)
-  , squareSize
-  ) where
+  (Square
+  ,rows
+  ,cols
+  ,fromList
+  ,create
+  ,alterF
+  ,(!))
+  where
 
-import           Control.Applicative
-import           Data.Coerce
-import           Data.Functor.Classes
-import           Data.Functor.Identity
-import           Data.Functor.Product
-import           Data.List
-import           Data.Monoid           hiding (Product (..))
-import           Data.Proxy
+import Control.Applicative
+import Data.Coerce
+import Data.Functor.Classes
+import Data.Functor.Identity
+import Data.Functor.Product
+import Data.Monoid hiding (Product(..))
+import Data.Proxy
+import Data.Foldable
+import Data.Semiring
+import GHC.TypeLits
+
+data Binary = Z | O Binary | I Binary
+
+type family Even (n :: Nat) (true :: k) (false :: k) :: k where
+  Even 0 true false = true
+  Even 1 true false = false
+  Even n true false = Even (n-2) true false
+
+type family Half (n :: Nat) :: Nat where
+  Half 0 = 0
+  Half 1 = 0
+  Half n = Half (n-2) + 1
+
+type family ToBinary (n :: Nat) :: Binary where
+  ToBinary 0 = 'Z
+  ToBinary n = (Even n 'O 'I) (ToBinary (Half n))
 
 -- | This type represents a square matrix. In the form:
 --
@@ -30,7 +59,7 @@ import           Data.Proxy
 -- <http://www.usma.edu/eecs/SiteAssets/SitePages/Faculty%20Publication%20Documents/Okasaki/icfp99square.pdf Okasaki's>
 -- square matrices:
 --
--- > data Square_ v w a =
+-- > data Square_ n v w a =
 -- >    Zero (v (v a))
 -- >  | Even (Square_      v     (Product w w) a)
 -- >  | Odd  (Square_ (Product v w) (Product w w) a)
@@ -40,37 +69,169 @@ import           Data.Proxy
 -- position. This could be accomplished without the field, but this
 -- way allows sharing of work across repeated access to different
 -- indices.
-data Square a =
-  Square { squareSize :: Int
-         , _square    :: Square_ Proxy Identity a
-         } deriving (Functor, Traversable)
+newtype Square n a =
+  Square { getSquare :: Square_ (ToBinary n) Proxy Identity a
+         } deriving (Functor, Foldable, Traversable, Eq, Ord, Eq1, Ord1)
 
-instance Foldable Square where
-  foldr f i (Square _ s) = foldr f i s
-  foldMap f (Square _ s) = foldMap f s
-  length    (Square n _) = n * n
+type Lens s a = ∀ f. Functor f => (a -> f a) -> s -> f s
 
-type Lens s a = forall f. Functor f => (a -> f a) -> s -> f s
+data Square_ n v w a where
+  Zero :: (∀ b. Int -> Lens (v b) b) -> v (v a) -> Square_ 'Z v w a
+  Even :: Square_ n          v    (Product w w) a   -> Square_ ('O n) v w a
+  Odd  :: Square_ n (Product v w) (Product w w) a   -> Square_ ('I n) v w a
 
-data Square_ v w a =
-    Zero (forall b. Int -> Lens (v b) b) (v (v a))
-  | Even (Square_          v    (Product w w) a)
-  | Odd  (Square_ (Product v w) (Product w w) a)
-  deriving (Functor)
+deriving instance (Functor v, Functor w) => Functor (Square_ n v w)
+deriving instance (Foldable v, Foldable w) => Foldable (Square_ n v w)
+deriving instance (Traversable v, Traversable w) => Traversable (Square_ n v w)
+
+newtype FreeMonoid a = FreeMonoid { runFree :: ∀ b. Monoid b => (a -> b) -> b }
+
+instance Monoid (FreeMonoid a) where
+  mempty = FreeMonoid (const mempty)
+  {-# INLINE mempty #-}
+  mappend (FreeMonoid x) (FreeMonoid y) = FreeMonoid (\f -> x f <> y f)
+  {-# INLINE mappend #-}
+
+instance Functor FreeMonoid where
+  fmap f (FreeMonoid fld) = FreeMonoid (\g -> fld (g . f))
+  {-# INLINE fmap #-}
+
+newtype Sequence f a = Sequence { unSequence :: f a } deriving (Functor, Applicative)
+
+instance (Applicative f, Monoid a) =>
+         Monoid (Sequence f a) where
+    mempty = Sequence (pure mempty)
+    {-# INLINE mempty #-}
+    mappend =
+        (coerce :: (f a -> f a -> f a) -> Sequence f a -> Sequence f a -> Sequence f a)
+            (liftA2 mappend)
+    {-# INLINE mappend #-}
+
+instance Applicative FreeMonoid where
+    pure x = FreeMonoid ($x)
+    FreeMonoid fs <*> FreeMonoid xs =
+        FreeMonoid
+            (\k ->
+                  fs
+                      (\c ->
+                            xs (k . c)))
+
+instance Foldable FreeMonoid where foldMap = flip runFree
+
+instance Traversable FreeMonoid where
+  traverse f (FreeMonoid xs) = (unSequence .# xs) (Sequence .# fmap pure . f)
+  {-# INLINE traverse #-}
+
+instance Show a => Show (FreeMonoid a) where
+  showsPrec n = showsPrec n . toList
+
+toFreeMonoid :: Foldable f => f a -> FreeMonoid a
+toFreeMonoid xs = FreeMonoid (`foldMap` xs)
+
+class Monoid1 f  where
+    liftMappend :: a -> (a -> a -> a) -> f a -> f a -> f a
+    liftEmpty :: a -> f a
+
+instance Monoid1 Identity where
+    liftMappend _ = coerce
+    liftEmpty = coerce
+    {-# INLINE liftMappend #-}
+    {-# INLINE liftEmpty #-}
+
+instance Monoid1 Proxy where
+    liftMappend _ _ _ _ = Proxy
+    liftEmpty _ = Proxy
+    {-# INLINE liftMappend #-}
+    {-# INLINE liftEmpty #-}
+
+instance (Monoid1 w, Monoid1 v) =>
+         Monoid1 (Product w v) where
+    liftMappend empt mapp (Pair w x) (Pair y z) =
+        Pair (liftMappend empt mapp w y) (liftMappend empt mapp x z)
+    liftEmpty e = Pair (liftEmpty e) (liftEmpty e)
+    {-# INLINE liftMappend #-}
+    {-# INLINE liftEmpty #-}
+
+mappend1 :: (Monoid1 f, Monoid a) => f a -> f a -> f a
+mappend1 = liftMappend mempty mappend
+{-# INLINE mappend1 #-}
+
+empty1 :: (Monoid1 f, Monoid a) => f a
+empty1 = liftEmpty mempty
+{-# INLINE empty1 #-}
+
+rows :: Square n a -> FreeMonoid (FreeMonoid a)
+rows = go toFreeMonoid . getSquare
+  where
+    go
+        :: (Foldable w, Foldable v, Functor v, Functor w)
+        => (v a -> FreeMonoid a) -> Square_ n v w a -> FreeMonoid (FreeMonoid a)
+    go f (Zero _ x) = toFreeMonoid (fmap f x)
+    go f (Even s) = go f s
+    go f (Odd s) = go g s
+      where
+        g (Pair vs ws) = f vs <> toFreeMonoid ws
+
+cols :: Square n a -> FreeMonoid (FreeMonoid a)
+cols = go . getSquare
+  where
+    go
+        :: (Foldable v, Foldable w, Functor v, Functor w, Monoid1 v, Monoid1 w)
+        => Square_ n v w a -> FreeMonoid (FreeMonoid a)
+    go (Zero _ x) = toFreeMonoid (foldr (mappend1 . fmap pure) empty1 x)
+    go (Even s) = go s
+    go (Odd s) = go s
+
+mulM
+    :: Semiring a
+    => Square n a -> Square n a -> FreeMonoid (FreeMonoid a)
+mulM x y = fmap (f . toList) (rows x)
+  where
+    f r = fmap (g r . toList) c
+    c = cols y
+    g rs cs = add (zipWith (<.>) rs cs)
+
+mulMat
+    :: (Semiring a, Create (ToBinary n))
+    => Square n a -> Square n a -> Square n a
+mulMat x y =
+    let Just res = (fromList . toList . fold)  (mulM x y)
+    in res
+
+combAdd :: (Monoid1 v, Semiring a) => v a -> v a -> v a
+combAdd = liftMappend zero (<+>)
+{-# INLINE combAdd #-}
+
+addMat :: (Semiring a) => Square n a -> Square n a -> Square n a
+addMat (Square x) (Square y) = Square (go x y)
+  where
+    go
+        :: (Functor v, Monoid1 v, Semiring a, Functor w, Monoid1 w)
+        => Square_ n v w a -> Square_ n v w a -> Square_ n v w a
+    go (Zero _ vx) (Zero ly vy) =
+        Zero ly (liftMappend (liftEmpty zero) combAdd vx vy)
+    go (Even xs) (Even ys) = Even (go xs ys)
+    go (Odd xs) (Odd ys) = Odd (go xs ys)
+
+instance (Semiring a, Create (ToBinary n), KnownNat n) => Semiring (Square n a) where
+  (<.>) = mulMat
+  (<+>) = addMat
+  one = evalUpd (0,0) (create (ones (fromInteger $ natVal (Proxy :: Proxy n))))
+  zero = runIdentity (create (Identity zero))
 
 -- | The 'create_' function is really two functions combined into one:
 -- a normal creation function, with the signature:
 --
--- >    (forall b. b -> v b)
--- > -> (forall b. b -> w b)
+-- >    (∀ b. b -> v b)
+-- > -> (∀ b. b -> w b)
 -- > -> a
 -- > -> Int
--- > -> Square_ v w a
+-- > -> Square_ n v w a
 --
 -- And an indexing function for a lens, with the signature:
 --
--- >    (forall b. Int -> Lens (v b) b)
--- > -> (forall b. Int -> Lens (w b) b)
+-- >    (∀ b. Int -> Lens (v b) b)
+-- > -> (∀ b. Int -> Lens (w b) b)
 -- > -> Int -> Int
 --
 -- building the indexing function allows for sharing between access
@@ -83,31 +244,44 @@ mkP = (liftA2.liftA2) Pair
 
 -- | Creates a square of side length @n@ from some applicative.
 -- >>> create 1 (Just 'a')
--- Just ["a"]
-create ::Applicative f => Int -> f a -> f (Square a)
-create n =
-  fmap (Square n) .
-  create_ leE leI 0 1 ((const.pure) Proxy) (fmap Identity) n
+  -- Just ["a"]
+create :: (Applicative f, Create (ToBinary n)) => f a -> f (Square n a)
+create =
+    fmap Square . create_ leE leI 0 1 ((const . pure) Proxy) (fmap Identity)
 
-create_ :: Applicative f
-        => (forall b. Int -> Lens (v b) b)
-        -> (forall b. Int -> Lens (w b) b)
-        -> Int -> Int
-        -> (forall b. f b -> f (v b))
-        -> (forall b. f b -> f (w b))
+
+class Create (n :: Binary)  where
+    create_
+        :: Applicative f
+        => (∀ b. Int -> Lens (v b) b)
+        -> (∀ b. Int -> Lens (w b) b)
         -> Int
+        -> Int
+        -> (∀ b. f b -> f (v b))
+        -> (∀ b. f b -> f (w b))
         -> f a
-        -> f (Square_ v w a)
-create_ lev _ _ _ mkv _ 0 = fmap (Zero lev) . mkv . mkv
-create_ lev lew vsz wsz mkv mkw n
-  | even n = fmap Even .
-    create_
-      lev (leP lew lew wsz) vsz (wsz+wsz)
-      mkv (mkP mkw mkw) (n `div` 2)
-  | otherwise = fmap Odd .
-    create_
-      (leP lev lew vsz) (leP lew lew wsz) (vsz+wsz) (wsz+wsz)
-      (mkP mkv mkw) (mkP mkw mkw) (n `div` 2)
+        -> f (Square_ n v w a)
+
+instance Create 'Z where
+    create_ lev _ _ _ mkv _ = fmap (Zero lev) . mkv . mkv
+
+instance Create n =>
+         Create ('O n) where
+    create_ lev lew vsz wsz mkv mkw =
+        fmap Even .
+        create_ lev (leP lew lew wsz) vsz (wsz + wsz) mkv (mkP mkw mkw)
+
+instance Create n =>
+         Create ('I n) where
+    create_ lev lew vsz wsz mkv mkw =
+        fmap Odd .
+        create_
+            (leP lev lew vsz)
+            (leP lew lew wsz)
+            (vsz + wsz)
+            (wsz + wsz)
+            (mkP mkv mkw)
+            (mkP mkw mkw)
 
 -- The indexing 'Lens for 'Proxy'. If this is called, it means an
 -- invalid index has been given. This is caught earlier with the
@@ -132,26 +306,27 @@ leP lev lew nv i f (Pair v w)
 -- Indexing
 ------------------------------------------------------------------------
 
-type FlippedLens s a = forall f. Functor f => s -> (a -> f a) -> f s
+type FlippedLens s a = ∀ f. Functor f => s -> (a -> f a) -> f s
 
-ix_ :: (Int, Int) -> Lens (Square_ v w a) a
+ix_ :: (Int, Int) -> Lens (Square_ n v w a) a
 ix_ (i,j) = flip ix' where
-  ix' :: FlippedLens (Square_ v w a) a
+  ix' :: FlippedLens (Square_ n v w a) a
   ix' (Zero lev vv) = \f -> Zero lev <$> (lev i . lev j) f vv
   ix' (Even      m) = fmap Even . ix' m
   ix' (Odd       m) = fmap Odd  . ix' m
 
 alterF
-  :: Applicative f
-  => (a -> f a) -> (Int, Int) -> Square a -> f (Square a)
-alterF f (i,j) s@(Square n q)
+  :: (Applicative f, KnownNat n)
+  => (a -> f a) -> (Int, Int) -> Square n a -> f (Square n a)
+alterF f (i,j) s@(Square q :: Square n a)
   | i <  0 = pure s
   | j <  0 = pure s
   | i >= n = pure s
   | j >= n = pure s
-  | otherwise = Square n <$> ix_ (i,j) f q
+  | otherwise = Square <$> ix_ (i,j) f q
+  where n = fromInteger (natVal (Proxy :: Proxy n))
 
-(!) :: Square a -> (Int, Int) -> Maybe a
+(!) :: KnownNat n => Square n a -> (Int, Int) -> Maybe a
 s ! i = (getFirst .# getConst) ((`alterF` i) (Const .# First .# Just) s)
 
 infixr 9 .#
@@ -159,73 +334,41 @@ infixr 9 .#
 (.#) _ = coerce
 
 ------------------------------------------------------------------------
--- Foldable and Traversable
-------------------------------------------------------------------------
-
-instance (Foldable v, Foldable w) => Foldable (Square_ v w) where
-  foldMap f (Zero _ v) = (foldMap.foldMap) f v
-  foldMap f (Even   v) = foldMap f v
-  foldMap f (Odd    v) = foldMap f v
-  foldr f i (Zero _ v) = foldr (flip $ foldr f) i v
-  foldr f i (Even   v) = foldr f i v
-  foldr f i (Odd    v) = foldr f i v
-
-instance (Traversable v, Traversable w)
-  => Traversable (Square_ v w) where
-    traverse f (Zero l v) = Zero l <$> (traverse.traverse) f v
-    traverse f (Even   v) = Even   <$> traverse            f v
-    traverse f (Odd    v) = Odd    <$> traverse            f v
-
-------------------------------------------------------------------------
 -- Eq and Ord
 ------------------------------------------------------------------------
 
-instance (Eq1 v, Eq1 w) => Eq1 (Square_ v w) where
-  liftEq eq (Zero _ x) (Zero _ y) = liftEq (liftEq eq) x y
-  liftEq eq (Odd    x) (Odd    y) = liftEq eq x y
-  liftEq eq (Even   x) (Even   y) = liftEq eq x y
-  liftEq _   _          _         = False
+instance (Eq1 v, Eq1 w) =>
+         Eq1 (Square_ n v w) where
+    liftEq eq (Zero _ x) (Zero _ y) = liftEq (liftEq eq) x y
+    liftEq eq (Odd x) (Odd y) = liftEq eq x y
+    liftEq eq (Even x) (Even y) = liftEq eq x y
 
-instance Eq a => Eq (Square a) where
-  Square n v == Square m w = n == m && eq1 v w
+instance (Eq1 v, Eq1 w, Eq a) =>
+         Eq (Square_ n v w a) where
+    (==) = eq1
+instance (Ord1 v, Ord1 w, Ord a) =>
+         Ord (Square_ n v w a) where
+    compare = compare1
 
-instance Eq1 Square where
-  liftEq eq (Square n v) (Square m w) = n == m && liftEq eq v w
-
-instance (Ord1 v, Ord1 w) => Ord1 (Square_ v w) where
-  liftCompare cmp (Zero _ x) (Zero _ y) =
-    (liftCompare.liftCompare) cmp x y
-  liftCompare cmp (Odd    x) (Odd    y) = liftCompare cmp x y
-  liftCompare cmp (Even   x) (Even   y) = liftCompare cmp x y
-  -- Differently sized squares are compared on their sizes
-  liftCompare _  _          _         = undefined
-
-instance Ord a => Ord (Square a) where
-  compare (Square n v) (Square m w) = compare n m <> compare1 v w
-
-instance Ord1 Square where
-  liftCompare cmp (Square n v) (Square m w) =
-    compare n m <> liftCompare cmp v w
-
-groupTo :: Int -> [a] -> [[a]]
-groupTo n = unfoldr $ \case
-  [] -> Nothing
-  xs -> Just (splitAt n xs)
+instance (Ord1 v, Ord1 w) =>
+         Ord1 (Square_ n v w) where
+    liftCompare cmp (Zero _ x) (Zero _ y) = (liftCompare . liftCompare) cmp x y
+    liftCompare cmp (Odd x) (Odd y) = liftCompare cmp x y
+    liftCompare cmp (Even x) (Even y) = liftCompare cmp x y
 
 ------------------------------------------------------------------------
 -- Show
 ------------------------------------------------------------------------
 
-instance Show a => Show (Square a) where
-  showsPrec n s =
-    showsPrec n . groupTo (squareSize s) . foldr (:) [] $ s
+instance Show a => Show (Square n a) where
+  showsPrec n s = showsPrec n (toList (fmap toList (rows s)))
 
 ------------------------------------------------------------------------
 -- fromList
 ------------------------------------------------------------------------
 
 newtype Source s a =
-  Source (forall c. (Maybe a -> List s -> c) -> List s -> c)
+  Source (∀ c. (Maybe a -> [s] -> c) -> [s] -> c)
 
 instance Functor (Source s) where
   fmap f (Source m) = Source (\t -> m (t . fmap f))
@@ -238,15 +381,38 @@ instance Applicative (Source s) where
     Source (\t -> fs (\f -> xs (t . (<*>) f)))
   {-# INLINABLE (<*>) #-}
 
-evalSource :: Source s a -> List s -> Maybe a
+evalSource :: Source s a -> [s] -> Maybe a
 evalSource (Source x) = x const
 {-# INLINABLE evalSource #-}
 
-newtype List a =
-  List (forall b. b -> (a -> List a -> b) -> b)
+fromList :: Create (ToBinary n) => [a] -> Maybe (Square n a)
+fromList = evalSource (create (Source uncons')) where
+  uncons' f [] = f Nothing []
+  uncons' f (x:xs) = f (Just x) xs
 
-fromList :: Foldable f => Int -> f a -> Maybe (Square a)
-fromList n = evalSource (create n pop) . foldr cons nil where
-  cons y ys = List (const (\g -> g y ys))
-  nil = List const
-  pop = Source (\t (List l) -> l (t Nothing nil) (t . Just))
+newtype Upd a =
+  Upd (∀ c. (a -> (Int,Int) -> c) -> (Int,Int) -> c)
+
+instance Functor Upd where
+  fmap f (Upd m) = Upd (\t -> m (t . f))
+  {-# INLINABLE fmap #-}
+
+instance Applicative Upd where
+  pure x = Upd (\t -> t x)
+  {-# INLINABLE pure #-}
+  Upd fs <*> Upd xs =
+    Upd (\t -> fs (\f -> xs (t . f)))
+  {-# INLINABLE (<*>) #-}
+
+ones :: (Semiring a) => Int -> Upd a
+ones n =
+    Upd
+        (\f (curcol,col) ->
+              if curcol == col
+                  then f one (curcol + 1, col)
+                  else if curcol == (n - 1)
+                           then f zero (0, col + 1)
+                           else f zero (curcol + 1, col))
+
+evalUpd :: (Int,Int) -> Upd a -> a
+evalUpd x (Upd f) = f const x
