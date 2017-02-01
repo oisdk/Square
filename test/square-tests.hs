b2c30d997af91a0e68155f5a8e1de6a807277067
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-02-01 18:43:35 +0000

    More

--- test/square-tests.hs
+++ test/square-tests.hs
@@ -10,7 +10,7 @@ import           Test.DocTest
 import           Test.QuickCheck
 import           Test.Semiring
 
-instance (Creatable n, Arbitrary a) => Arbitrary (Square n a) where
+instance (Creatable n, Arbitrary a, KnownDimension c) => Arbitrary (Square c n a) where
   arbitrary = create arbitrary
 
 instance Testable (Either String a) where
@@ -24,13 +24,14 @@ type Ternary a  = a -> a -> a -> Either String String
 
 main :: IO ()
 main = do
-    quickCheck (unaryLaws :: Unary (Square 2 Integer))
-    quickCheck (binaryLaws :: Binary (Square 2 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 2 Integer))
-    quickCheck (unaryLaws :: Unary (Square 3 Integer))
-    quickCheck (binaryLaws :: Binary (Square 3 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 3 Integer))
-    quickCheck (unaryLaws :: Unary (Square 9 Integer))
-    quickCheck (binaryLaws :: Binary (Square 9 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 9 Integer))
+    quickCheck (unaryLaws   :: Unary   (Square 2 2 Integer))
+    quickCheck (binaryLaws  :: Binary  (Square 2 2 Integer))
+    quickCheck (ternaryLaws :: Ternary (Square 2 2 Integer))
+    quickCheck (unaryLaws   :: Unary   (Square 2 3 Integer))
+    quickCheck (unaryLaws   :: Unary   (Square 4 2 Integer))
+    quickCheck (binaryLaws  :: Binary  (Square 2 3 Integer))
+    quickCheck (ternaryLaws :: Ternary (Square 2 3 Integer))
+    quickCheck (unaryLaws   :: Unary   (Square 2 9 Integer))
+    quickCheck (binaryLaws  :: Binary  (Square 2 9 Integer))
+    quickCheck (ternaryLaws :: Ternary (Square 2 9 Integer))
     doctest ["-isrc", "src/Data/Square.hs"]
