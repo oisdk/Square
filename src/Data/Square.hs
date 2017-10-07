d041dfc69fe24fdcafcb6dba3532cdad5e9cec82
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-10-07 17:08:59 +0100

    fixed tobinary

--- src/Data/Square.hs
+++ src/Data/Square.hs
@@ -62,20 +62,20 @@ type family EnclosedBy' (cmp :: Ordering) (p :: Nat) (n :: Nat) :: Nat where
     EnclosedBy' 'EQ p n = EnclosedBy (p + 1) n
     EnclosedBy' 'LT p n = EnclosedBy (p + 1) n
 
-type family ToBinary' (p :: Nat) (n :: Nat) :: Binary where
-    ToBinary' 0 n = 'Z
-    ToBinary' p n = ToBinary'' (2 ^ (p - 1)) p n
+type family ToBinary' (b :: Binary) (p :: Nat) (n :: Nat) :: Binary where
+    ToBinary' b 0 n = b
+    ToBinary' b p n = ToBinary'' b (2 ^ (p - 1)) p n
 
-type family ToBinary'' (below :: Nat) (p :: Nat) (n :: Nat) :: Binary where
-    ToBinary'' below p n = ToBinary''' below (CmpNat n below) p n
+type family ToBinary'' (b :: Binary) (below :: Nat) (p :: Nat) (n :: Nat) :: Binary where
+    ToBinary'' b below p n = ToBinary''' b below (CmpNat n below) p n
 
-type family ToBinary''' (below :: Nat) (curr :: Ordering) (p :: Nat) (n :: Nat) :: Binary where
-    ToBinary''' below 'GT p n = 'I (ToBinary' (p-1) (n - below))
-    ToBinary''' below 'EQ p n = 'I (ToBinary' (p-1) (n - below))
-    ToBinary''' below 'LT p n = 'O (ToBinary' (p-1) n)
+type family ToBinary''' (b :: Binary) (below :: Nat) (curr :: Ordering) (p :: Nat) (n :: Nat) :: Binary where
+    ToBinary''' b below 'GT p n = ToBinary' ('I b) (p-1) (n - below)
+    ToBinary''' b below 'EQ p n = ToBinary' ('I b) (p-1) (n - below)
+    ToBinary''' b below 'LT p n = ToBinary' ('O b) (p-1) n
 
 type family ToBinary (n :: Nat) :: Binary where
-    ToBinary n = ToBinary' (EnclosedBy 0 n) n
+    ToBinary n = ToBinary' 'Z (EnclosedBy 0 n) n
 
 -- | This type represents a square matrix. In the form:
 --
