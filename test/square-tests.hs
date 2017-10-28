45e269dbb902d78716dbd23e339b77f535ba9e4e
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-10-28 14:34:16 +0100

    rearranged

--- test/square-tests.hs
+++ test/square-tests.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators     #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE DataKinds            #-}
 
 module Main (main) where
 
 import           Data.Square
+import           Data.Square.Internal.Binary
+import           Data.Type.Equality
 import           Test.DocTest
 import           Test.QuickCheck
 import           Test.Semiring
@@ -18,19 +21,32 @@ instance Testable (Either String a) where
   property (Left s)  = counterexample s False
 
 
-type Unary a  = a -> Either String String
-type Binary a  = a -> a -> Either String String
-type Ternary a  = a -> a -> a -> Either String String
+type UnaryFun a  = a -> Either String String
+type BinaryFun a  = a -> a -> Either String String
+type TernaryFun a  = a -> a -> a -> Either String String
+
+_toBinaryTests :: ()
+_toBinaryTests = () where
+  _one :: ToBinary 1 :~: 'I 'Z
+  _one = Refl
+  _zero :: ToBinary 0 :~: 'Z
+  _zero = Refl
+  _two :: ToBinary 2 :~: 'O ('I 'Z)
+  _two = Refl
+  _six :: ToBinary 6 :~: 'O ('I ('I 'Z))
+  _six = Refl
+  _sixHundred :: ToBinary 600 :~: 'O ('O ('O ('I ('I ('O ('I ('O ('O ('I 'Z)))))))))
+  _sixHundred = Refl
 
 main :: IO ()
 main = do
-    quickCheck (unaryLaws :: Unary (Square 2 Integer))
-    quickCheck (binaryLaws :: Binary (Square 2 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 2 Integer))
-    quickCheck (unaryLaws :: Unary (Square 3 Integer))
-    quickCheck (binaryLaws :: Binary (Square 3 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 3 Integer))
-    quickCheck (unaryLaws :: Unary (Square 9 Integer))
-    quickCheck (binaryLaws :: Binary (Square 9 Integer))
-    quickCheck (ternaryLaws :: Ternary (Square 9 Integer))
+    quickCheck (unaryLaws :: UnaryFun (Square 2 Integer))
+    quickCheck (binaryLaws :: BinaryFun (Square 2 Integer))
+    quickCheck (ternaryLaws :: TernaryFun (Square 2 Integer))
+    quickCheck (unaryLaws :: UnaryFun (Square 3 Integer))
+    quickCheck (binaryLaws :: BinaryFun (Square 3 Integer))
+    quickCheck (ternaryLaws :: TernaryFun (Square 3 Integer))
+    quickCheck (unaryLaws :: UnaryFun (Square 9 Integer))
+    quickCheck (binaryLaws :: BinaryFun (Square 9 Integer))
+    quickCheck (ternaryLaws :: TernaryFun (Square 9 Integer))
     doctest ["-isrc", "src/Data/Square.hs"]
